b00d80597fe8f761225f392bc819c2dc5844b3b2
fardin23-admin <frafat@hollandcollege.com>
2025-10-28 00:02:24 -0300

    SPRINT 2- added changes to the database

--- Project/perfume_sales_tracker/src/main/java/info/hccis/bus/pass/TheApplication.java
+++ Project/perfume_sales_tracker/src/main/java/info/hccis/bus/pass/TheApplication.java
@@ -6,8 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TheApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TheApplication.class, args);
-	}
+    public static void main(String[] args) {
+
+
+        SpringApplication.run(TheApplication.class, args);
+    }
 
 }
